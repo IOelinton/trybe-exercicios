03eeb41a60d9e960cf7f2733089ee4771a5a2c38
Oelinton Araujo <oelinton.dearaujo@gmail.com>
2022-06-30 17:34:11 -0300

    dias anteriores

---
+++ fundamentos/bloco-05-dom-e-seletores/dia04-01/exercicios-aula/script.js
@@ -0,0 +1,2 @@
+let quadro = document.getElementsByClassName('no-emergency-tasks')[0].style.background= "yellow";
+quadro = document.getElementsByClassName('no-emergency-tasks')[0].style.color= "black";
